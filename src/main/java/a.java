03f5731220fd361e2d4c2e5272168d1c9c2ff03b
wuyongkang <2216855594@qq.com>
2021-11-26 14:18:16 +0800

    经典工作代码

---
+++ src/main/java/a.java
@@ -0,0 +1,14 @@
+/**
+ * @author wuyongkang
+ * @date 2021年11月25日 17:51
+ */
+public class a {
+    public static void main(String[] args) {
+        for (int i = 0; i <6 ; i++) {
+            if(i==3){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
