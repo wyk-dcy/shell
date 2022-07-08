2915fa43922a3eaa148f3adef4a04a8ad8a0d709
wuyongkang <2216855594@qq.com>
2022-07-08 20:16:45 +0800

    你属

--- src/main/java/event/EventConsumer.java
+++ src/main/java/event/EventConsumer.java
@@ -4,7 +4,7 @@ public class EventConsumer implements ValueChangeListener {
 
     @Override
     public void performed(ValueChangeEvent valueChangeEvent) {
-        System.out.println("value changed, new value 新建的= " + ((Stu)valueChangeEvent.getValue()).getName());
-        System.out.println("value changed, new value = " + ((Stu)valueChangeEvent.getValue()).getName());
+        System.out.println("value changed, new value 新建的= ");
+        System.out.println("value changed is wyk, new value = " + ((Stu)valueChangeEvent.getValue()).getName());
     }
 }
