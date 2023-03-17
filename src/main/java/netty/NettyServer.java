3563766ecf91367dfee19224ff254a8ff292571a
wuyongkang <2216855594@qq.com>
2023-03-17 17:38:52 +0800

    去除无用代码

--- src/main/java/netty/NettyServer.java
+++ src/main/java/netty/NettyServer.java
@@ -61,14 +61,6 @@ public class NettyServer {
 
     public static void main(String[] args) throws InterruptedException {
         new NettyServer(10086);
-        String i = "add";
-        String b = "merge";
 
-        String c = "merge";
-        String d = "merge";
-        String gg= "新的测试哦";
-
-        String cg= "wuyongkang";
-        String dg= "dochunyazn";
     }
 }
