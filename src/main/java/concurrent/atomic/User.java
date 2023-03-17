0be2faa9ea0e270643c2dbb4f51ee513315335c1
wuyongkang <2216855594@qq.com>
2023-03-17 17:45:55 +0800

    添加bro

--- src/main/java/concurrent/atomic/User.java
+++ src/main/java/concurrent/atomic/User.java
@@ -6,9 +6,23 @@ package concurrent.atomic;
  */
 public class User {
     private int age;
+    private int bar;
+
+    public int getBar() {
+        return bar;
+    }
+
+    public void setBar(int bar) {
+        this.bar = bar;
+    }
 
     public int getAge() {
         return age;
+    }
+
+    public User(int age, int bar) {
+        this.age = age;
+        this.bar = bar;
     }
 
     public void setAge(int age) {
