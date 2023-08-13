eea080d401d646da69a2e9153a81df704f4d4c5c
wuyongkang <2216855594@qq.com>
2023-08-13 17:18:31 +0800

    提价

--- src/main/java/swing/SwingTest.java
+++ src/main/java/swing/SwingTest.java
@@ -25,7 +25,7 @@ public class SwingTest {
                             }
                         }
                     }
-                });
+                });cdvdv
                 thread.start();
             }
         });
